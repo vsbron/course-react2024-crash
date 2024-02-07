ab41a027b86d7a9c4494f52ead7891283fa6a6cd
vsbron <vsbron@gmail.com>
2024-02-07 15:21:36 +0300

    Fact's tag BG gets added from the separate array

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 const initialFacts = [
   {
     id: 1,
@@ -71,8 +82,12 @@ function createFactsList(data) {
   const htmlArr = data.map(
     (fact) => `
     <li class="fact">
-      <p>${fact.text}<a href="${fact.source}" class="source" target="_blank">(Source)</a></p>
-      <span class="tag" style="background: #eab308">${fact.category}</span>
+      <p>${fact.text}<a href="${
+      fact.source
+    }" class="source" target="_blank">(Source)</a></p>
+      <span class="tag" style="background: ${
+        CATEGORIES.find((cat) => cat.name === fact.category).color
+      }">${fact.category}</span>
       <div class="vote-buttons">
         <button>👍 ${fact.votesInteresting}</button>
         <button>🤯 ${fact.votesMindblowing}</button>
