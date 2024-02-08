3b7dfd274468d335dcd5d138d313fb963a6a709b
vsbron <vsbron@gmail.com>
2024-02-08 13:01:00 +0300

    Added css, header and the fonts from previous version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" width="68" height="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn--large btn--open">Share a fact</button>
+    </header>
   );
 }
 
