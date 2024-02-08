39e814a1a36fc18507a6e21696e2efe4c122950e
vsbron <vsbron@gmail.com>
2024-02-08 14:34:21 +0300

    Facts categories are renedered from the utils file list

--- src/Components/CategoryFilter.jsx
+++ src/Components/CategoryFilter.jsx
@@ -1,5 +1,25 @@
+import { CATEGORIES } from "../utils/constants";
+
 function CategoryFilter() {
-  return <aside>Category filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li>
+          <button className="btn btn--all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li className="category" key={cat.name}>
+            <button
+              className="btn btn--category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default CategoryFilter;
