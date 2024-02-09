5eeb5a20eb631c9718ce9e56f2a528964105f351
vsbron <vsbron@gmail.com>
2024-02-09 14:59:15 +0300

    Added dummy submit handler to the Form

--- src/Components/NewFactForm.jsx
+++ src/Components/NewFactForm.jsx
@@ -7,15 +7,26 @@ function NewFactForm() {
   const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
 
+  // Variables for fact's maximum length counter
+  const maxLength = 200;
+  const textLength = maxLength - text.length;
+
+  // Form submit handler
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
   return (
-    <form className="fact-form">
+    <form className="fact-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Share a fact with the world..."
         value={text}
         onChange={(e) => setText(e.target.value)}
+        maxLength={maxLength}
       />
-      <span>999</span>
+      <span>{textLength}</span>
 
       <input
         type="text"
