aaf3d8fab22d04c96b5617c2486e31ca19f01b1b
vsbron <vsbron@gmail.com>
2024-02-09 14:54:15 +0300

    Form fields are now Controlled React Form Elements

--- src/Components/NewFactForm.jsx
+++ src/Components/NewFactForm.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { CATEGORIES } from "../utils/constants";
 
 function NewFactForm() {
+  // Creating states for controlled form elements
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+
   return (
     <form className="fact-form">
-      <input type="text" placeholder="Share a fact with the world..." />
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <span>999</span>
-      <input type="text" placeholder="Trustworthy source..." />
-      <select>
+
+      <input
+        type="text"
+        placeholder="Trustworthy source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
         <option value="">Choose category:</option>
         {CATEGORIES.map((cat) => (
           <option key={cat.name} value={cat.name}>
