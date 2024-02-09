8be1413879e3a883eb6ded88e0968d1c99d1e366
vsbron <vsbron@gmail.com>
2024-02-09 14:48:05 +0300

    Form now lists the categories dynamicly from the constants

--- src/Components/NewFactForm.jsx
+++ src/Components/NewFactForm.jsx
@@ -1,5 +1,22 @@
+import { CATEGORIES } from "../utils/constants";
+
 function NewFactForm() {
-  return <form className="fact-form">Fact form</form>;
+  return (
+    <form className="fact-form">
+      <input type="text" placeholder="Share a fact with the world..." />
+      <span>999</span>
+      <input type="text" placeholder="Trustworthy source..." />
+      <select>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name[0].toUpperCase() + cat.name.slice(1)}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn--large">Post</button>
+    </form>
+  );
 }
 
 export default NewFactForm;
