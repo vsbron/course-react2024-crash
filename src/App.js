eaf1ae72e438804c6dbde978c7e5b21eda0f1fd9
vsbron <vsbron@gmail.com>
2024-02-12 13:27:18 +0300

    Added Loader component that is visible when the data is loaded. Changed the file structure a little bit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import { useEffect, useState } from "react";
 
-import supabase from "./supabase";
+import supabase from "./services/supabase";
 
 import CategoryFilter from "./components/CategoryFilter";
 import FactList from "./components/FactList";
 import Header from "./components/Header";
 import NewFactForm from "./components/NewFactForm";
 
+import Loader from "./ui/Loader";
+
 import "./style.css";
 
 function App() {
-  // Creating state for form visibility and fact list
+  // Creating state for form visibility, fact list and loading state
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Use effect hook that loads the initial facts
   useEffect(() => {
     async function getFacts() {
+      // Enabling Loading state
+      setIsLoading(true);
+
+      // Getting the data
       let { data: facts, error } = await supabase.from("facts").select("*");
-      setFacts(facts);
+
+      // Updating the Facts in the state if there's no Errors
+      if (!error) setFacts(facts);
+      else alert("There was a problem getting the data");
+
+      // Disabling the Loading state
+      setIsLoading(false);
     }
 
     getFacts();
@@ -32,7 +45,7 @@ function App() {
       )}
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} />
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
       </main>
     </>
   );
